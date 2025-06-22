c83978737034336e2ea1c77717de09aa5115403d
Saba Motto <sabapotcom@yahoo.co.jp>
2025-06-23 00:03:33 +0900

    refactor: migrate v20 and vuepress v2

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,47 @@
+import { defineUserConfig } from 'vuepress'
+import { defaultTheme } from '@vuepress/theme-default'
+import { viteBundler } from '@vuepress/bundler-vite'
+
+export default defineUserConfig({
+  bundler: viteBundler(),
+  theme: defaultTheme({
+    navbar: [
+      {
+        text: 'Apps',
+        link: '/apps/',
+      },
+      {
+        text: 'About us',
+        link: '/about/'
+      }
+    ],
+    sidebar: {
+      '/apps/': [
+        {
+          text: 'Apps',
+          collapsible: false,
+          children: [
+            '/apps/README.md',
+          ]
+        }
+      ],
+    }
+  }),
+
+  title: 'Popprate LLC',
+  description: 'The corporate site of Popprate LLC.',
+
+  head: [
+    ['meta', { name: 'theme-color', content: '#44c0c2' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['script', { async: true, src: 'https://www.googletagmanager.com/gtag/js?id=G-GF8Z9BMW5M' }],
+    ['script', {}, `
+      window.dataLayer=window.dataLayer||[];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js',new Date());gtag('config','G-GF8Z9BMW5M');
+    `]
+  ],
+
+  plugins: []
+})
